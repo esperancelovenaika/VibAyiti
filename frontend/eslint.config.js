3ac015c132cc99fb2e1e29ddeb1497912b9a9fee
esperancelovenaika <esperancelovenaika07@gmail.com>
2026-06-26 14:14:42 -0400

    Add frontend eslint.config.js

---
+++ frontend/eslint.config.js
@@ -0,0 +1,32 @@
+import react from 'eslint-plugin-react'
+import eslintRecommended from 'eslint-plugin-react/configs/recommended'
+
+export default [
+  eslintRecommended,
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 2021,
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+    plugins: {
+      react,
+    },
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      'react/prop-types': 'off',
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    },
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+  },
+]
